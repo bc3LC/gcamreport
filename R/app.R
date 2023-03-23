047be23bd55a19491c21be5fa1215836c9bb0f50
klau506 <claudia.rodes@bc3research.org>
2023-03-23 13:08:54 +0100

    Draft of gcamreport app

--- R/app.R
+++ R/app.R
@@ -1,60 +1,60 @@
-# # Load packages ----------------------------------------------------------------
-#
-# library(shiny)
-# library(ggplot2)
-# library(dplyr)
-#
-# # Load data --------------------------------------------------------------------
-#
-# load("movies.RData")
-#
-# # Define UI --------------------------------------------------------------------
-#
-# ui <- fluidPage(
-#   sidebarLayout(
-#
-#     sidebarPanel(
-#
-#       radioButtons(inputId = "filetype",
-#                    label = "Select filetype:",
-#                    choices = c("csv", "tsv"),
-#                    selected = "csv"),
-#
-#       checkboxGroupInput(inputId = "selected_var",
-#                          label = "Select variables:",
-#                          choices = names(movies),
-#                          selected = c("title"))
-#
-#     ),
-#
-#     mainPanel(
-#       HTML("Select filetype and variables, then hit 'Download data'."),
-#       downloadButton("download_data", "Download data")
-#     )
-#   )
-# )
-#
-# # Define server ----------------------------------------------------------------
-#
-# server <- function(input, output, session) {
-#
-#   # Download file
-#   output$download_data <- downloadHandler(
-#     filename = function() {
-#       paste0("movies.", input$filetype)
-#     },
-#     content = function(file) {
-#       if(input$filetype == "csv"){
-#         write_csv(movies %>% select(input$selected_var), file)
-#       }
-#       if(input$filetype == "tsv"){
-#         write_tsv(movies %>% select(input$selected_var), file)
-#       }
-#     }
-#   )
-#
-# }
-#
-# # Create the Shiny app object --------------------------------------------------
-#
-# shinyApp(ui = ui, server = server)
+# User friendly interface to interact with.
+
+library(usethis)
+library(magrittr)
+
+# Load data --------------------------------------------------------------------
+
+if (!exists('final_data')) {
+  gcamreport::load_project('gas_fin_updated')
+  gcamreport::read_queries(final_db_year = 2050)
+}
+
+# Define UI --------------------------------------------------------------------
+
+ui <- fluidPage(
+  sidebarLayout(
+
+    sidebarPanel(
+
+      checkboxGroupInput(inputId = "selected_var",
+                         label = "Select variables:",
+                         choices = unique(final_data$Variable),
+                         selected = unique(final_data$Variable))
+
+    ),
+
+    mainPanel(
+      DT::dataTableOutput(outputId = "datatable"),
+      downloadButton("download_data", "Download data")
+    )
+  )
+)
+
+# Define server ----------------------------------------------------------------
+
+server <- function(input, output, session) {
+
+  # Data table
+  output$datatable <- DT::renderDataTable({
+    data_sample <- final_data
+    DT::datatable(data = data_sample,
+                  options = list(pageLength = 10),
+                  rownames = FALSE)
+  })
+
+  # Download file
+  output$download_data <- downloadHandler(
+    filename = function() {
+      paste0("gcamreport.csv")
+    },
+    content = function(file) {
+      write.csv(final_data, file)
+    }
+  )
+
+}
+
+# Create the Shiny app object --------------------------------------------------
+
+shinyApp(ui = ui, server = server)
