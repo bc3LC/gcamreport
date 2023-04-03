b18f759c314572d7e3b0d83e0829c56dd53ad025
klau506 <claudia.rodes@bc3research.org>
2023-04-03 09:36:36 +0200

    Integrate app with dashboards into R pkg

--- inst/shiny-examples/gcamreport_app/server.R
+++ inst/shiny-examples/gcamreport_app/server.R
@@ -3,111 +3,140 @@ library(magrittr)
 library(shiny)
 library(shinyTree)
 
-# server <- shinyServer(function(input, output, session) {
-#   addClass(selector = "body", class = "sidebar-collapse")
-# })
-#
-
 # Define server ----------------------------------------------------------------
 
-server <- function(input, output, session) {
+server <- function(input, output) {
+  ## -- debug
+  output$res3 <- renderPrint(input$tree_regions)
 
-  # Debugging
-  #########
-  # output$debug <- renderPrint({
-  #   sel_tree = shinyTree::get_selected(input$tree, format = 'slices')
-  #   save(sel_tree, file = file.path('C:\\Users\\claudia.rodes\\Documents\\IAM_COMPACT\\gcamreport\\tt.RData'))
-  #
-  #   print(sel_tree)
-  # })
-  # output$debug2 <- renderPrint({
-  # sel_tree_reg = shinyTree::get_selected(input$tree_regions, format = 'slices')
-  # sel_tree_reg = do_unmount_tree(sel_tree_reg, 'regions')
-  # save(sel_tree_reg, file = file.path('C:\\Users\\claudia.rodes\\Documents\\IAM_COMPACT\\gcamreport\\reg.RData'))
 
-  # print(unique(data_sample$Regions))
-  # })
-  ############
-
-  # Select all/none variables
-  treeDataVar_sel <- reactive({
-    tree_vars <- do_mount_tree(cols, names(cols), selec = TRUE)
-  })
-  treeDataVar_unsel <- reactive({
-    tree_vars <- do_mount_tree(cols, names(cols), selec = FALSE)
-  })
+  ## -- select all/none variables
   observeEvent(input$select_all_variables, {
-    updateTree(session, treeId = "tree_variables", data = treeDataVar_sel())
+    updateTreeInput(inputId = "tree_variables", selected = c(unique(cols[,1])))
   })
   observeEvent(input$select_none_variables, {
-    updateTree(session, treeId = "tree_variables", data = treeDataVar_unsel())
+    updateTreeInput(inputId = "tree_variables", selected = character(0))
   })
 
 
-  # Select all/none regions
-  treeDataReg_sel <- reactive({
-    tree_reg <- do_mount_tree(reg_cont, names(reg_cont), selec = TRUE)
-  })
-  treeDataReg_unsel <- reactive({
-    tree_reg <- do_mount_tree(reg_cont, names(reg_cont), selec = FALSE)
-
-  })
+  ## -- select all/none variables
   observeEvent(input$select_all_regions, {
-    updateTree(session, treeId = "tree_regions", data = treeDataReg_sel())
+    updateTreeInput(inputId = "tree_regions", selected = c(unique(reg_cont$region)))
   })
   observeEvent(input$select_none_regions, {
-    updateTree(session, treeId = "tree_regions", data = treeDataReg_unsel())
+    updateTreeInput(inputId = "tree_regions", selected = character(0))
   })
 
 
-  # Variables tree
-  output$tree_variables <- shinyTree::renderTree({
-    tree_vars
-  })
-  # Regions tree
-  output$tree_regions <- shinyTree::renderTree({
-    tree_reg
-  })
-
-
-  # Subset selected by the user
-  doo_data_sample <- reactive({
-    do_data_sample(sdata,input$selected_scen,input$selected_years,input$selected_cols,
-                   shinyTree::get_selected(input$tree_variables, format = 'slices'),
-                   shinyTree::get_selected(input$tree_regions, format = 'slices'))
-  })
-
-  # Plot
-  output$plot <- renderPlot({
-    data_sample = doo_data_sample()
-    data_sample = tidyr::pivot_longer(data_sample, cols = 6:ncol(data_sample), names_to = 'year', values_to = 'values') %>%
-      dplyr::mutate(values = as.numeric(as.character(values))) %>%
-      dplyr::mutate(year = as.numeric(as.character(year)))
-
-    ggplot2::ggplot(data = data_sample, ggplot2::aes(x = year, y = values, color = Scenario, linetype = Variable, group = interaction(Scenario,Region,Variable))) +
-      ggplot2::geom_point(ggplot2::aes(shape = Region)) +
-      ggplot2::geom_line() +
-      ggplot2::scale_color_manual('Scenario', values = viridis::magma(length(unique(data_sample$Scenario)))) +
-      ggplot2::scale_linetype_manual('Variables', values = rep(c(1:9), times = ceiling(length(unique(data_sample$Variable))/9))) +
-      ggplot2::labs(title = paste0('Evolution of ', unique(data_sample$Variable)), y = unique(data_sample$Unit), x = 'Year')
-  })
-
-  # Data table
+  ## -- data table
   output$datatable <- DT::renderDataTable({
-    DT::datatable(data = doo_data_sample(),
-                  options = list(pageLength = 10),
+    DT::datatable(data = do_data_sample(sdata,
+                                        input$selected_scen,input$selected_years,
+                                        input$selected_cols,input$tree_variables,
+                                        input$tree_regions),
+                  options = list(pageLength = 10, scrollX = TRUE),
                   rownames = FALSE)
   })
 
-  # Download file
-  output$download_data <- downloadHandler(
+
+  ## -- plot
+  observe({
+    if (input$graph_grouping == 'Grouped'){
+      # display one single plot with all selected variables
+
+      # insert the right number of plot output objects into the web page
+      output$plots <- renderUI({
+        plot_output_list <- lapply(1:1, function(i) {
+          plotname <- paste("plot", i, sep="")
+          plotOutput(plotname, height = 400, width = 1000)
+        })
+
+        # Convert the list to a tagList - this is necessary for the list of items
+        # to display properly.
+        do.call(tagList, plot_output_list)
+      })
+
+      local({
+        my_i <- 1
+        plotname <- paste("plot", my_i, sep="")
+
+        output[[plotname]] <- renderPlot({
+          data_sample = do_data_sample(sdata,
+                                       input$selected_scen,input$selected_years,
+                                       input$selected_cols,input$tree_variables,
+                                       input$tree_regions)
+          data_sample = tidyr::pivot_longer(data_sample, cols = 6:ncol(data_sample), names_to = 'year', values_to = 'values') %>%
+            dplyr::mutate(values = as.numeric(as.character(values))) %>%
+            dplyr::mutate(year = as.numeric(as.character(year)))
+          ggplot2::ggplot(data = data_sample, ggplot2::aes(x = year, y = values, color = Scenario, linetype = Variable, group = interaction(Scenario,Region,Variable))) +
+            ggplot2::geom_point(ggplot2::aes(shape = Region)) +
+            ggplot2::geom_line() +
+            ggplot2::scale_linetype_manual('Variables', values = rep(c(1:9), times = ceiling(length(unique(data_sample$Variable))/9))) +
+            ggplot2::scale_color_manual('Scenario', values = rainbow(length(unique(data_sample$Scenario)))) +
+            ggplot2::labs(title = paste0('Evolution of ', unique(data_sample$Variable)), y = unique(data_sample$Unit), x = 'Year')
+        })
+      })
+    }
+    else if (input$graph_grouping == 'Ungrouped') {
+      # display one plot for each variable
+
+      # insert the right number of plot output objects into the web page
+      output$plots <- renderUI({
+        plot_output_list <- lapply(1:length(input$tree_variables), function(i) {
+          plotname <- paste("plot", i, sep="")
+          plotOutput(plotname, height = 400, width = 1000)
+        })
+
+        # Convert the list to a tagList - this is necessary for the list of items
+        # to display properly.
+        do.call(tagList, plot_output_list)
+      })
+
+
+      for (i in 1:100) {
+        # Need local so that each item gets its own number. Without it, the value
+        # of i in the renderPlot() will be the same across all instances, because
+        # of when the expression is evaluated.
+        local({
+          my_i <- i
+          plotname <- paste("plot", my_i, sep="")
+
+          output[[plotname]] <- renderPlot({
+            data_sample = do_data_sample(sdata,
+                                         input$selected_scen,input$selected_years,
+                                         input$selected_cols,input$tree_variables[my_i],
+                                         input$tree_regions)
+            data_sample = tidyr::pivot_longer(data_sample, cols = 6:ncol(data_sample), names_to = 'year', values_to = 'values') %>%
+              dplyr::mutate(values = as.numeric(as.character(values))) %>%
+              dplyr::mutate(year = as.numeric(as.character(year)))
+
+            ggplot2::ggplot(data = data_sample, ggplot2::aes(x = year, y = values, color = Scenario, linetype = Variable, group = interaction(Scenario,Region,Variable))) +
+              ggplot2::geom_point(ggplot2::aes(shape = Region)) +
+              ggplot2::geom_line() +
+              ggplot2::guides(linetype = 'none') +
+              ggplot2::scale_linetype_manual('', values = rep(c(1:9), times = ceiling(length(unique(data_sample$Variable))/9))) +
+              ggplot2::scale_color_manual('Scenario', values = rainbow(length(unique(data_sample$Scenario)))) +
+              ggplot2::labs(title = paste0('Evolution of ', unique(data_sample$Variable)), y = unique(data_sample$Unit), x = 'Year')
+          })
+
+        })
+      }
+    }
+  })
+
+
+  ## -- download button
+  output$downloadData <- downloadHandler(
     filename = function() {
-      paste0("gcamreport.csv")
+      paste('data-', Sys.Date(), '.csv', sep='')
     },
-    content = function(file) {
-      write.csv(doo_data_sample(), file)
+    content = function(con) {
+      write.csv(do_data_sample(sdata,
+                               input$selected_scen,input$selected_years,
+                               input$selected_cols,input$tree_variables,
+                               input$tree_regions),
+                con)
     }
   )
 
 }
-
--- inst/shiny-examples/gcamreport_app/ui.R
+++ inst/shiny-examples/gcamreport_app/ui.R
@@ -1,87 +1,176 @@
-library(usethis)
-library(magrittr)
 library(shiny)
-library(shinyTree)
+library(shinyWidgets)
 library(shinydashboard)
+library(shinyTree)
+library(magrittr)
 library(shinyjs)
+library(dashboardthemes)
 
 # Define UI --------------------------------------------------------------------
 
-ui <- fluidPage(
-  titlePanel("GCAMREPORT user interface",
-             windowTitle = 'gcamreport'),
-
-  sidebarLayout(
-    sidebarPanel(
-      actionButton(inputId = 'hidePanel',
-                   label = 'Hide panel'),
-
-      conditionalPanel(
-        condition = "input.hidePanel%2 == 0",
+ui <- dashboardPage(
+  dashboardHeader(title = "gcamreport"),
+  dashboardSidebar(sidebarMenu(
 
 
-        checkboxGroupInput(inputId = "selected_scen",
-                           label = "Select scenarios:",
-                           choices = unique(sdata$Scenario),
-                           selected = unique(sdata$Scenario)),
-
-        br(),
-        tags$div(style="font-weight: bold", checked=NA,
-                 tags$p("Select variables")
-        ),
-        shinyTree("tree_variables",
-                  checkbox = TRUE,
-                  search = TRUE,
-                  searchtime = 500),
-        actionLink("select_all_variables","Select All"),
-        actionLink("select_none_variables","Select None"),
-
-        br(),br(),
-        tags$div(style="font-weight: bold", checked=NA,
-                 tags$p("Select regions")
-        ),
-        # tags$div(class="header", checked=NA,
-        #          tags$p("Select regions")
-        # ),
-
-        shinyTree("tree_regions",
-                  checkbox = TRUE),
-        actionLink("select_all_regions","Select All"),
-        actionLink("select_none_regions","Select None"),
-
-        br(),br(),
-        checkboxGroupInput(inputId = "selected_years",
-                           label = "Select years:",
-                           inline = TRUE,
-                           choices = available_years,
-                           selected = available_years),
-
-        checkboxGroupInput(inputId = "selected_cols",
-                           label = "Select columns:",
-                           choices = c('Model',
-                                       'Scenario',
-                                       'Region',
-                                       'Variable',
-                                       'Unit'),
-                           selected = c('Model',
-                                        'Scenario',
-                                        'Region',
-                                        'Variable',
-                                        'Unit')),
+    ## -- Scenarios
+    menuItem(
+      text = "Scenarios",
+      icon = NULL,
+      startExpanded = FALSE,
+      menuItem(
+        awesomeCheckboxGroup(
+          inputId = "selected_scen",
+          label = "Select scenarios",
+          choices = unique(sdata$Scenario),
+          selected = unique(sdata$Scenario)
+        )
       )
     ),
 
-    mainPanel(
-      tabsetPanel(
-        type = "tabs",
+
+    ## -- Regions
+    menuItem(
+      text = "Regions",
+      icon = NULL,
+      startExpanded = FALSE,
+      menuItem(
+        treeInput(
+          inputId = "tree_regions",
+          label = "Select regions:",
+          choices = shinyWidgets::create_tree(reg_cont,
+                                              levels = c('continent','region'),
+                                              levels_id = c('code_continent','code_region')),
+          selected = "Africa",
+          returnValue = "id",
+          closeDepth = 0
+        )
+      ),
+      menuItem(
+        actionBttn(
+          inputId = "select_all_regions",
+          label = "Select all",
+          style = "minimal",
+          size = 'xs'
+        )
+      ),
+      menuItem(
+        actionBttn(
+          inputId = "select_none_regions",
+          label = "Select none",
+          style = "minimal",
+          size = 'xs'
+        )
+      ),
+      br()
+    ),
+
+
+    ## -- Variables
+    menuItem(
+      text = "Variables",
+      icon = NULL,
+      startExpanded = FALSE,
+      menuItem(
+        treeInput(
+          inputId = "tree_variables",
+          label = "Select variables:",
+          choices = shinyWidgets::create_tree(cols,
+                                              levels = c('col1','col2','col3','col4','col5','col6','col7'),
+                                              levels_id = c('code_col1','code_col2','code_col3','code_col4','code_col5','code_col6','code_col7')),
+          selected = "Agricultural Demand",
+          returnValue = "id",
+          closeDepth = 0
+        )
+      ),
+      menuItem(
+        actionBttn(
+          inputId = "select_all_variables",
+          label = "Select all",
+          style = "minimal",
+          size = 'xs'
+        )
+      ),
+      menuItem(
+        actionBttn(
+          inputId = "select_none_variables",
+          label = "Select none",
+          style = "minimal",
+          size = 'xs'
+        )
+      ),
+      br()
+    ),
+
+
+    ## -- Years
+    menuItem(
+      text = "Years",
+      icon = NULL,
+      startExpanded = FALSE,
+      menuItem(
+        awesomeCheckboxGroup(
+          inputId = "selected_years",
+          label = "Select years",
+          choices = available_years,
+          selected = available_years
+        )
+      )
+    ),
+
+
+    ## -- Columns
+    menuItem(
+      text = "Columns",
+      icon = NULL,
+      startExpanded = FALSE,
+      menuItem(
+        awesomeCheckboxGroup(
+          inputId = "selected_cols",
+          label = "Select columns",
+          choices = c('Model',
+                      'Scenario',
+                      'Region',
+                      'Variable',
+                      'Unit'),
+          selected = c('Model',
+                       'Scenario',
+                       'Region',
+                       'Variable',
+                       'Unit')
+        )
+      )
+    ),
+
+
+    ## -- Download button
+    downloadBttn(
+      outputId = "downloadData",
+      style = "simple",
+      color = "default",
+      size = 'sm'
+    )
+  )),
+
+
+  dashboardBody(
+    fluidRow(
+      tabBox(
+        width = 12,
+        id = "tab_box",
         tabPanel("Data",
-                 # textOutput(outputId = "debug"),
-                 # textOutput(outputId = "debug2"),
-                 DT::dataTableOutput(outputId = "datatable"),
-                 downloadButton("download_data", "Download data")
+                 # verbatimTextOutput("res3"),
+                 DT::dataTableOutput(outputId = "datatable")
         ),
         tabPanel("Plot",
-                 plotOutput("plot"))
+                 radioGroupButtons(
+                   inputId = "graph_grouping",
+                   label = "Choose how the graph variables should be displayed: ",
+                   choices = c("Grouped", "Ungrouped")
+                 ),
+                 br(),
+                 # dynamic UI for the plots
+                 uiOutput("plots"))
       )
     )
   )
