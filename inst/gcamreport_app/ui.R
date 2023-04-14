c675762c0e475744b682e1fc8f5591306fbbf395
klau506 <claudia.rodes@bc3research.org>
2023-04-14 12:02:34 +0200

    Delete unnecessary pkgs

--- inst/gcamreport_app/ui.R
+++ inst/gcamreport_app/ui.R
@@ -4,7 +4,6 @@ library(shinyWidgets)
 library(shinydashboard)
 library(shinyTree)
 library(shinyjs)
-library(dashboardthemes)
 
 # Define some variables --------------------------------------------------------
 
