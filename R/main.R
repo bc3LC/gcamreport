a4a52ec7746d69efc5ddddb398596468dba19bdb
klau506 <claudia.rodes@bc3research.org>
2023-07-22 12:51:36 +0200

    Fix CO2 prices 2

--- R/main.R
+++ R/main.R
@@ -80,18 +80,15 @@ fill_queries = function(db_path, db_name, prj_name, scenarios) {
   # fix CO2 prices if needed
   if (!'CO2 prices' %in% rgcam::listQueries(prj)) {
     l = length(rgcam::listScenarios(prj))
-    for (sc in rgcam::listScenarios(prj)) {
-      dt = data.frame(Units = rep(NA,l),
-                      scenario = sc,
-                      year = rep(NA,l),
-                      market = rep(NA,l),
-                      value = rep(NA,l))
-      prj_tmp <- rgcam::addQueryTable(project = prj_name, qdata = dt,
-                                      queryname = 'CO2 prices', clobber = TRUE)
-      prj <<- rgcam::mergeProjects(prj_name, list(prj,prj_tmp), clobber = TRUE, saveProj = FALSE)
-    }
+    dt = data.frame(Units = rep(NA,l),
+                    scenario = rgcam::listScenarios(prj),
+                    year = rep(NA,l),
+                    market = rep(NA,l),
+                    value = rep(NA,l))
+    prj_tmp <- rgcam::addQueryTable(project = prj_name, qdata = dt,
+                                    queryname = 'CO2 prices', clobber = TRUE)
+    prj <<- rgcam::mergeProjects(prj_name, list(prj,prj_tmp), clobber = TRUE, saveProj = FALSE)
   }
-
 }
 
 
