b04468465a26a3dd08a6f194771c74cee5d872f9
Andy M <andy.miller331@gmail.com>
2025-07-18 15:38:10 -0400

    Add necessary wood and cereal datafiles for non-SMIP versions

--- inst/extdata/saveDataFiles_GCAM7.1.R
+++ inst/extdata/saveDataFiles_GCAM7.1.R
@@ -116,6 +116,19 @@ L100.AgMIP_FoodWaste_Share_Pathway_SSP_v7.1 <- readr::read_csv(file.path(rawData
   dplyr::select(-GCAM_region_ID)
 use_data(L100.AgMIP_FoodWaste_Share_Pathway_SSP_v7.1, overwrite = T)
 
+# wood fuel - industrial roundwood
+WoodFuel_IndRoundwood_ratio_v7.1 <- readr::read_csv(file.path(rawDataFolder, "inst/extdata/mappings/GCAM7.1", "FAO_GCAM_Ratio_WoodFuel_IndRoundwood_R_Yh.csv"),
+                                                    comment = "#"
+)
+use_data(WoodFuel_IndRoundwood_ratio_v7.1, overwrite = T)
+
+# cereal yield and land scalar
+cereal_scaler_v7.1 <- readr::read_csv(file.path(rawDataFolder, "inst/extdata/mappings/GCAM7.1", "GCAM_Yield_R_Cereals_Scaler_Y.csv"),
+                                      comment = "#"
+)
+use_data(cereal_scaler_v7.1, overwrite = T)
+
+
 
 # primary, secondary, final energy maps
 primary_energy_map_v7.1 <- readr::read_csv(file.path(rawDataFolder, "inst/extdata/mappings/GCAM7.1", "primary_energy_map.csv"),
