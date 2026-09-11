ebcc8bf056e4313177df9662be0f4580e72b6f7d
klau506 <klau506@users.noreply.github.com>
2026-09-11 08:36:14 +0000

    Built site for gcamreport@1.0.3: ecc20d1

--- R/gcamreport.R
+++
@@ -1,9 +0,0 @@
-#' gcamreport
-#'
-#' \itemize{
-#' \item Github: https://github.com/bc3LC/gcamreport
-#' \item Webpage: https://bc3lc.github.io/gcamreport/}
-#'
-#' @docType _PACKAGE
-#' @name gcamreport
-NULL
--- R/zzz.R
+++
@@ -1,8 +0,0 @@
-.myGlobals <- new.env(parent = emptyenv())
-
-.onLoad <- function(libname, pkgname) {
-  .myGlobals$ignore.global <- NULL
-  .myGlobals$interactive.global <- NULL
-  .myGlobals$variables.global <- NULL
-  .myGlobals$GCAM_version <- NULL
-}
